905f9d7deddd2e39fc89bcabd62faece12518de0
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2024-02-23 22:19:06 +0100

    ...

--- components/receiptCard.vue
+++ components/receiptCard.vue
@@ -3,7 +3,9 @@
     <div v-for="card in store.receipts">
       <div class="bg-slate-50 shadow-lg rounded-lg text-sm p-[1em]">
         <div class="text-gray-400">
-          <p class="text-base font-semibold border-b border-slate-400 pb-[0.5em] mb-[0.5em] text-blue-400">{{ card.customerName }}</p>
+          <p class="text-base font-semibold border-b border-slate-400 pb-[0.5em] mb-[0.5em] text-blue-400">
+            {{ card.customerName }}
+          </p>
           <p class="my-[0.5em]">{{ card.productName }}</p>
           <p class="my-[0.5em]">QTY: {{ card.productQuantity }}</p>
           <p class="my-[0.5em]">PRICE: N{{ card.productPrice }}</p>
@@ -20,11 +22,8 @@
 <script setup>
 import { useStore } from "../store"
 
-const router = useRouter()
+// const router = useRouter()
 const store = useStore()
-onMounted(() => {
-  store.fetchReceipts()
-})
 
 const viewR = async() => {
   
