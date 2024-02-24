1d3d25ccb34eb74d08a3f9ed2b52d3fcfef26a44
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2024-02-24 09:14:27 +0100

    fix: cleared array when refetching

--- store.ts
+++ store.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import type { RuleForm } from './types'
-import { collection, getDocs, addDoc, setDoc, doc } from "firebase/firestore"
+import { collection, getDocs, addDoc, setDoc, doc, query, where, onSnapshot } from "firebase/firestore"
 
 
 export const useStore = defineStore('receipts', {
@@ -26,6 +26,7 @@ export const useStore = defineStore('receipts', {
 
     async fetchReceipts() {
       const nuxtApp = useNuxtApp()
+      this.receipts = [];
       const querySnapshot = await getDocs(collection(nuxtApp.$firestore, "receipts"))
       querySnapshot.forEach((doc) => {
         this.receipts.push({...doc.data(), id: doc.id} as RuleForm)
