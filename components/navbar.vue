3cc390db0795739cbb99f4897d2a81c2648a29d8
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2024-02-28 16:46:10 +0100

    ...

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="shadow-md fixed top-0 bg-white w-[100%]">
     <div class="flex justify-between py-[1em] px-[1em]">
-      <NuxtLink class="flex gap-x-[0.5em]" to="/">
+      <NuxtLink class="flex gap-x-[0.5em]" to="/dashboard">
         <img class="w-[2em]" src="/public/snapbill-logo.png" />
         <p class="font-bold text-lg text-sky-600">snapB!LL</p>
       </NuxtLink>
