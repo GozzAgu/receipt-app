3b264526dd24c88bb224a6d9528142725a2b98d9
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2024-03-03 08:18:58 +0100

    fix: moved animated loader to own component

--- components/receipts/receiptCards.vue
+++ components/receipts/receiptCards.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="py-[6em] px-[1em] md:px-[5em] lg:px-[15em] relative">
-    <!-- <div class="loader"></div>
-    <p class="p-[0.5em] my-[1em] text-center text-red-300 font-thin">
-      app in progress...
-    </p> -->
     <h1 class="font-semibold text-xl text-gray-500">
       <Icon name="material-symbols:receipt-long-outline" color="gray" size="25" />
       <span class="">Receipts</span>
@@ -146,74 +142,8 @@ onMounted(() => {
 })
 </script>
 
-<style >
+<style scoped>
 .el-table tr {
   @apply text-[0.75em] md:text-[0.9em]
-}
-
-.loader {
-  width: 28px;
-  height: 28px;
-  margin: auto;
-  position: relative;
-}
-
-.loader:before {
-  content: '';
-  width: 28px;
-  height: 5px;
-  background: #c5631350;
-  position: absolute;
-  top: 40px;
-  left: 0;
-  border-radius: 50%;
-  animation: shadow324 0.5s linear infinite;
-}
-
-.loader:after {
-  content: '';
-  width: 100%;
-  height: 100%;
-  background: #f08080;
-  position: absolute;
-  top: 0;
-  left: 0;
-  border-radius: 4px;
-  animation: jump7456 0.5s linear infinite;
-}
-
-@keyframes jump7456 {
-  15% {
-    border-bottom-right-radius: 3px;
-  }
-
-  25% {
-    transform: translateY(9px) rotate(22.5deg);
-  }
-
-  50% {
-    transform: translateY(18px) scale(1, .9) rotate(45deg);
-    border-bottom-right-radius: 40px;
-  }
-
-  75% {
-    transform: translateY(9px) rotate(67.5deg);
-  }
-
-  100% {
-    transform: translateY(0) rotate(90deg);
-  }
-}
-
-@keyframes shadow324 {
-
-  0%,
-    100% {
-    transform: scale(1, 1);
-  }
-
-  50% {
-    transform: scale(1.2, 1);
-  }
 }
 </style>
