aad4cbda0e518f51f2adc73e07a00d1897fc7787
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2024-03-18 07:43:23 +0100

    ...

--- components/dashboard/body.vue
+++ components/dashboard/body.vue
@@ -1,17 +1,50 @@
 <template>
-  <div class="px-[1em] md:px-[5em] lg:px-[5em] mt-[3rem] relative">
-    <div class="">
-      <p class="font-semibold">Recent swaps</p>
-      <div class="overflow-y-auto h-[15rem] px-[2rem] py-[1.5rem] border rounded-lg shadow-md">
-        <div v-for="(receipt, index) in filteredReceipts.slice(0, 10)" :key="index">
-          <div class="text-sm border rounded-lg p-[0.5rem] mb-[0.3rem]">
-            <div class="flex justify-between">
-              <p class="font-semibold">{{ receipt.customerName }}</p>
-              <p>{{ receipt.productName }}</p>
-            </div>
-            <p class="text-end text-sky-600">₦{{ receipt.productPrice }}</p>
-          </div>
+  <div class="px-[1em] pb-[3rem] md:px-[5em] lg:px-[5em] mt-[3rem] relative">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-[3rem]">
+      <div class="border p-[2rem] rounded-lg h-[25rem]">
+        <div class="flex gap-x-[0.5rem] mb-[0.5rem]">
+          <Icon class="text-2xl text-sky-600" name="ic:sharp-swap-horiz" />
+          <p class="font-semibold text-sky-600">Recent swaps</p>
         </div>
+        <el-table 
+          :default-sort="{ prop: 'date', order: 'descending' }" 
+          v-if="store.receipts.length > 0" 
+          :data="filteredReceipts" 
+          height="95%"
+          style="width: 100%; max-height: 100%;"
+        > 
+          <el-table-column prop="swapFrom" label="SWAP FROM" width="130"  show-overflow-tooltip>
+            <template #default="scope">
+              {{ scope.row.swapFrom }} 
+            </template>
+          </el-table-column>
+          <el-table-column prop="productName" label="TO" width="130" show-overflow-tooltip />
+          <el-table-column prop="customerName" label="CUSTOMER" width="130" show-overflow-tooltip />
+          <el-table-column prop="date" sortable label="DATE" width="100"  show-overflow-tooltip />
+        </el-table>
+      </div>
+
+      <div class="border p-[2rem] rounded-lg h-[25rem]">
+        <div class="flex gap-x-[0.5rem] mb-[0.5rem]">
+          <Icon class="text-2xl text-green-500" name="ic:twotone-loyalty" />
+          <p class="font-semibold text-green-500">Patrons</p>
+        </div>
+        <el-table 
+          :default-sort="{ prop: 'date', order: 'descending' }" 
+          v-if="store.receipts.length > 0" 
+          :data="filteredReceipts" 
+          height="95%"
+          style="width: 100%; max-height: 100%;"
+        > 
+          <el-table-column prop="customerName" label="CUSTOMER" width="130" show-overflow-tooltip />
+          <el-table-column prop="productName" label="PRODUCT" width="130" show-overflow-tooltip />
+          <el-table-column prop="newPrice" label="PRICE" width="130" show-overflow-tooltip>
+            <template #default="scope">
+              ₦{{ scope.row.newPrice }} 
+            </template>
+          </el-table-column>
+          <el-table-column prop="date" sortable label="DATE" width="100"  show-overflow-tooltip />
+        </el-table>
       </div>
     </div>
   </div>
@@ -32,3 +65,21 @@ onMounted(() =>{
   store.fetchReceipts()
 })
 </script>
+
+<style scoped>
+::-webkit-scrollbar {
+  width: 8px;
+}
+
+::-webkit-scrollbar-track {
+  background: #f1f1f1
+}
+
+::-webkit-scrollbar-thumb {
+  @apply bg-gray-400
+}
+
+::-webkit-scrollbar-thumb:hover {
+  @apply bg-gray-300
+}
+</style>
--- components/dashboard/welcomeCard.vue
+++ components/dashboard/welcomeCard.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="pt-[5.5em] px-[1em] md:px-[5em] lg:px-[5em] relative">
-    <div class="grid grid-cols-1 md:grid-cols-2">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-[3rem]">
       <div class="px-[2rem] py-[1rem] border rounded-lg shadow-md">
         <p class="font-semibold lg:w-[70%]">
           Welcome to snapB!LL, <span class="font-bold">{{ authStore.currentUser?.email }}</span> 
         </p>
         <div class="flex justify-end">
-          <p class="mt-[0.5rem] text-sm border border-green-400 bg-green-100 rounded-lg w-[4rem] text-center px-[0.5rem] py-[0.1rem]">
+          <p class="mt-[0.5rem] text-sm text-green-500 border border-green-400 bg-green-100 rounded-lg w-[4rem] text-center px-[0.5rem] py-[0.1rem]">
             {{ authStore.currentUser?.accountType }}
           </p>
         </div>  
       </div>
+      <!-- <div class="grid grid-cols-2">
+        <el-statistic title="Total Receipts" :value="outputValue" />
+      </div> -->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useAuthStore } from '~/store/users'
+import { useStore } from '~/store/receipts'
 
+const route = useRoute()
 const authStore = useAuthStore()
+const store = useStore()
+const receipts = ref(0)
+
+watch(() => store.receipts.length, (newLength) => {
+  receipts.value = newLength
+})
+
+watch(() => store.receipts.length, (newLength) => {
+  receipts.value = newLength
+})
+
+const outputValue = useTransition(receipts, {
+  duration: 1500,
+})
 </script>
