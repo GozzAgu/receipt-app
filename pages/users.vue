0f3ac80fe0fa810123681754b4f0c0097dbd2d5d
GozAgu <115640033+GozAgu@users.noreply.github.com>
2024-04-11 20:14:03 -1100

    fix

--- pages/users.vue
+++ pages/users.vue
@@ -5,9 +5,13 @@
 </template>
 
 <script setup>
+import { useAuthStore } from '~/store/users'
+
 definePageMeta({
   layout:'dashboard'
 })
+
+const authStore = useAuthStore()
 
 onMounted(() => {
   authStore.authenticated()
